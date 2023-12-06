2cd1a5ba71fb90214d77583a1f48cfd54c93e314
Alessio Rimoldi <alessio.rimoldi.work@gmail.com>
2023-12-06 17:53:32 +0100

    added canvas for setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Your-Package-Name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+    ],
+    entry_points={
+        'console_scripts': [
+            'your-script = your_package.module:main_function',
+        ],
+    },
+    # metadata to display on PyPI
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="This is an Example Package",
+    keywords="hello world example examples",
+    url="http://example.com/HelloWorld/",   # project home page, if any
+)
